3371e9a743c3578634af03ff1e52959384f2fd5e
Takuya Mukohira <takuya.mk96@gmail.com>
2016-03-12 15:50:47 +0900

    Implement E-mail transmission using SendGrid

--- lib/notifiers/email-notifier.rb
+++ lib/notifiers/email-notifier.rb
@@ -1,39 +1,44 @@
 require 'yaml'
-require 'pony'
+require 'sendgrid-ruby'
 
 class EmailNotifier
   def initialize(news, config)
     @news = news
-    @config = config
+    @email_config = config
   end
 
   def notify
-    if @news.category == "news"
-      body = @news.body + "\n"
-      body << @news.link + "\n"
-    else @news.category == "info"
-      body = @news.title + "\n"
-      body << @news.link + "\n"
-    end
+    client = SendGrid::Client.new(api_key: @email_config["sendgrid_api_key"])
 
     to_addresses = YAML.load_file('./config/to_addresses.yml')
-    to_addresses['to_addresses'].to_a.each do |to_address|
-      Pony.mail(
-        to: to_address,
-        subject: "[苫小牧高専News] #{@news.title}",
-        body: body,
-        charset: 'utf-8',
-        via: :smtp,
-        via_options: {
-          enable_starttls_auto: @config['server']['enable_starttls_auto'],
-          address: @config['server']['address'],
-          port: @config['server']['port'],
-          user_name: @config['server']['user_name'],
-          password: @config['server']['password'],
-          authentication: @config['server']['authentication'],
-          domain: @config['server']['domain']
-        }
-      )
+    header = Smtpapi::Header.new
+    header.add_to(to_addresses)
+
+    if @news.category == "news"
+      text_body = @news.body + "\n"
+      text_body << @news.link + "\n"
+    else @news.category == "info"
+      text_body = @news.title + "\n"
+      text_body << @news.link + "\n"
     end
+
+    html_body = text_body.split.map { |p|
+      if p.include?("http://")
+        "<p><a href=\"#{p}\">#{p}</a></p>"
+      else
+        "<p>#{p}</p>"
+      end
+    }.join
+
+    mail = SendGrid::Mail.new do |m|
+      m.to = @email_config["from"]
+      m.from = @email_config["from"]
+      m.from_name = @email_config["from_name"]
+      m.subject = "[苫小牧高専News] #{@news.title}"
+      m.text = text_body
+      m.html = html_body
+      m.smtpapi = header
+    end
+    client.send(mail)
   end
 end
